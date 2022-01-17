b83156139dacf8b84cd566406b77a1bbc015b166
rvpatil1432 <rvpatil1432@gmail.com>
2022-01-17 14:59:58 +0530

    Added : Use case to calculate employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,13 +1,32 @@
-/*
- * UC1 Check Employee Present or Absent
- */
-{
-    const IS_ABSENT = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 2;
-    if (empCheck == IS_ABSENT) {
-        console.log("Employee is absent");
-        return;
-    } else {
-        console.log("Employee is present");
-    }
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+ // UC1 Check Employee Present or Absent 
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee is absent");
+    return;
+} else {
+    console.log("Employee is present");
 }
+
+ // UC2 Calculating Employee Wage
+    let empHrs = 0;
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    switch (empCheck) {
+        case IS_PART_TIME:
+            empHrs = PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            empHrs = FULL_TIME_HOURS;
+            break;
+        default:
+            empHrs = 0;
+    }
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("Employee Wage:" + empWage);
