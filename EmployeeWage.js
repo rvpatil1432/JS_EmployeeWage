a7d9fdcebaed8c8c2bde74ef1136f51829079475
rvpatil1432 <rvpatil1432@gmail.com>
2022-01-17 19:52:19 +0530

    Added : Use case to calculate monthly employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,6 +4,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -15,7 +16,10 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log(" Emp Hour:" + empHrs + " Emp Wage: " + empWage);
+let totalEmpHrs = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Hours:" + totalEmpHrs + " Employee Wage:" + empWage);
