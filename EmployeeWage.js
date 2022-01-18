5d6953f7a08cbc7dfce3772e4c58dbeb1f6bd13f
rvpatil1432 <rvpatil1432@gmail.com>
2022-01-18 08:00:34 +0530

    Added : Use case to calculate employee wage and store daily wage into array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,6 +6,7 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
 const MAX_HRS_IN_MONTH = 160;
+let empDailyWageArr = new Array();
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -17,12 +18,19 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
+/** function for calculating daily wage */
+function calculateDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calculateDailyWage(empHrs));
 }
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Total Days:" + totalWorkingDays + " Total Hours:" + totalEmpHrs + " Employee Wage:" + empWage);
+console.log("Total Days:" + totalWorkingDays + "," + " Total Hours:" + totalEmpHrs );
+ console.log("Employee Wage: " + empWage + "," + "Daily Wages: " + "[" + empDailyWageArr + "]");
