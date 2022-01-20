38fcdd31574e1b5c59d85627ac8bdb963ab20ea0
rvpatil1432 <rvpatil1432@gmail.com>
2022-01-20 07:45:55 +0530

    Added : Use case to extend Employee Payroll to add employee gender and DOJ.

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -3,12 +3,16 @@ class EmployeePayrollData {
     //property
     id;
     salary;
+    gender;
+    startDate;
 
     //constructor
-    constructor(id, name, salary) {
+    constructor(id, name, salary,gender,startDate) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
     }
 
     //getter and setter method
@@ -18,13 +22,23 @@ class EmployeePayrollData {
     set name(name) {
         this._name = name;
     }
-
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
     //method
     toString() {
-        return "id =" + this.id + ", name = " + this.name + ", salary = " + this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "undefined" :
+            this.startDate.toLocaleDateString("en-US", options);
+        return "id =" + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + empDate;
     }
 }
 let employeePayrollData = new EmployeePayrollData(1, "Rohini", 500000);
 console.log(employeePayrollData);
 employeePayrollData.name = "Neeta";
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(2, "Terissa", 400000, "F", new Date());
+console.log(newEmployeePayrollData.toString()); 
